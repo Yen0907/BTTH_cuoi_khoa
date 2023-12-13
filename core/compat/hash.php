23f9f1b157e8dc351332d74f99bd8cc6b4c635fc
Nhi <149870091+nhinhi20@users.noreply.github.com>
2023-12-14 03:21:41 +0700

    hash.php

--- core/compat/hash.php
+++ core/compat/hash.php
@@ -1,72 +1,15 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 3.0.0
- * @filesource
- */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-/**
- * PHP ext/hash compatibility package
- *
- * @package		CodeIgniter
- * @subpackage	CodeIgniter
- * @category	Compatibility
- * @author		Andrey Andreev
- * @link		https://codeigniter.com/user_guide/
- * @link		http://php.net/hash
- */
-
-// ------------------------------------------------------------------------
 
 if (is_php('5.6'))
 {
 	return;
 }
 
-// ------------------------------------------------------------------------
-
 if ( ! function_exists('hash_equals'))
 {
-	/**
-	 * hash_equals()
-	 *
-	 * @link	http://php.net/hash_equals
-	 * @param	string	$known_string
-	 * @param	string	$user_string
-	 * @return	bool
-	 */
+
 	function hash_equals($known_string, $user_string)
 	{
 		if ( ! is_string($known_string))
@@ -94,29 +37,16 @@ if ( ! function_exists('hash_equals'))
 	}
 }
 
-// ------------------------------------------------------------------------
 
 if (is_php('5.5'))
 {
 	return;
 }
 
-// ------------------------------------------------------------------------
 
 if ( ! function_exists('hash_pbkdf2'))
 {
-	/**
-	 * hash_pbkdf2()
-	 *
-	 * @link	http://php.net/hash_pbkdf2
-	 * @param	string	$algo
-	 * @param	string	$password
-	 * @param	string	$salt
-	 * @param	int	$iterations
-	 * @param	int	$length
-	 * @param	bool	$raw_output
-	 * @return	string
-	 */
+
 	function hash_pbkdf2($algo, $password, $salt, $iterations, $length = 0, $raw_output = FALSE)
 	{
 		if ( ! in_array(strtolower($algo), hash_algos(), TRUE))
@@ -227,7 +157,6 @@ if ( ! function_exists('hash_pbkdf2'))
 		}
 
 		$hash = '';
-		// Note: Blocks are NOT 0-indexed
 		for ($bc = ceil($length / $hash_length), $bi = 1; $bi <= $bc; $bi++)
 		{
 			$key = $derived_key = hash_hmac($algo, $salt.pack('N', $bi), $password, TRUE);
@@ -239,7 +168,6 @@ if ( ! function_exists('hash_pbkdf2'))
 			$hash .= $derived_key;
 		}
 
-		// This is not RFC-compatible, but we're aiming for natural PHP compatibility
 		return substr($raw_output ? $hash : bin2hex($hash), 0, $length);
 	}
 }
