ec4bcbbf4dbefdbbde31a9583d0ea18a6e94a4a2
Nhi <149870091+nhinhi20@users.noreply.github.com>
2023-12-14 04:22:26 +0700

    oci8_forge.php

--- database/drivers/oci8/oci8_forge.php
+++ database/drivers/oci8/oci8_forge.php
@@ -1,96 +1,19 @@
 <?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.4.1
- * @filesource
- */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * Oracle Forge Class
- *
- * @category	Database
- * @author		EllisLab Dev Team
- * @link		https://codeigniter.com/user_guide/database/
- */
 class CI_DB_oci8_forge extends CI_DB_forge {
 
-	/**
-	 * CREATE DATABASE statement
-	 *
-	 * @var	string
-	 */
 	protected $_create_database	= FALSE;
 
-	/**
-	 * CREATE TABLE IF statement
-	 *
-	 * @var	string
-	 */
 	protected $_create_table_if	= FALSE;
 
-	/**
-	 * DROP DATABASE statement
-	 *
-	 * @var	string
-	 */
 	protected $_drop_database	= FALSE;
 
-	/**
-	 * DROP TABLE IF statement
-	 *
-	 * @var	string
-	 */
 	protected $_drop_table_if	= FALSE;
 
-	/**
-	 * UNSIGNED support
-	 *
-	 * @var	bool|array
-	 */
 	protected $_unsigned		= FALSE;
 
-	// --------------------------------------------------------------------
-
-	/**
-	 * ALTER TABLE
-	 *
-	 * @param	string	$alter_type	ALTER type
-	 * @param	string	$table		Table name
-	 * @param	mixed	$field		Column definition
-	 * @return	string|string[]
-	 */
 	protected function _alter_table($alter_type, $table, $field)
 	{
 		if ($alter_type === 'DROP')
@@ -141,30 +64,11 @@ class CI_DB_oci8_forge extends CI_DB_forge {
 		return $sqls;
 	}
 
-	// --------------------------------------------------------------------
-
-	/**
-	 * Field attribute AUTO_INCREMENT
-	 *
-	 * @param	array	&$attributes
-	 * @param	array	&$field
-	 * @return	void
-	 */
 	protected function _attr_auto_increment(&$attributes, &$field)
 	{
-		// Not supported - sequences and triggers must be used instead
+
 	}
 
-	// --------------------------------------------------------------------
-
-	/**
-	 * Field attribute TYPE
-	 *
-	 * Performs a data type mapping between different databases.
-	 *
-	 * @param	array	&$attributes
-	 * @return	void
-	 */
 	protected function _attr_type(&$attributes)
 	{
 		switch (strtoupper($attributes['TYPE']))
